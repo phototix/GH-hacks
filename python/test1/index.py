bc2a147f367bb6f66680352a7c8daafe9f147f89
Brandon Chong <phototix@hotmail.com>
2023-07-20 17:55:50 +0800

    Update

---
+++ python/test1/index.py
@@ -0,0 +1,8 @@
+"""
+This is also a multi-line comment.
+It can span multiple lines and is often used as a docstring.
+"""
+# This is also comment in one line.
+
+# Try to out put something.
+print("Hello, World!")
