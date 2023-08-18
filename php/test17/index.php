e884162cce88efc2c9c47e3d358945da67919be3
Brandon Chong <phototix@hotmail.com>
2023-08-18 16:30:59 +0800

    Update formular

--- php/test17/index.php
+++ php/test17/index.php
@@ -1,22 +1,17 @@
 <?php
 
-function voltageDivider(float $Vin, float $R1, float $R2) {
-    $Vd = $Vin * ($R2 / ($R1 + $R2));
-    return $Vd;
-}
+// Get the input values
+$ds = $_POST['ds'];
+$nhr = $_POST['nhr'];
+$nvr = $_POST['nvr'];
+$cvr = $_POST['cvr'];
 
-// Validate and retrieve input values from $_POST
-$Vin = isset($_POST['vin']) ? floatval($_POST['vin']) : null;
-$R1 = isset($_POST['r1']) ? floatval($_POST['r1']) : null;
-$R2 = isset($_POST['r2']) ? floatval($_POST['r2']) : null;
+// Calculate the viewing distance
+$vd = 2.54 * 100 / (tan(1/60) * ($nhr * $nvr + 1) * $cvr);
 
-if ($Vin !== null && $R1 !== null && $R2 !== null) {
-    // Calculate the output voltage
-    $Vd = voltageDivider($Vin, $R1, $R2);
-    
-    // Display the output voltage
-    echo "The output voltage is: $Vd volts\n";
-}
+// Display the viewing distance
+echo "The viewing distance is " . $vd . " meters.";
+
 ?>
 
 <!DOCTYPE html>
@@ -25,17 +20,12 @@ if ($Vin !== null && $R1 !== null && $R2 !== null) {
     <title>Voltage Divider Calculator</title>
 </head>
 <body>
-    <form method="post">
-        <label for="vin">Enter the input voltage (V): </label>
-        <input type="number" step="any" name="vin" required><br>
-        
-        <label for="r1">Enter the value of resistor R1 (ohms): </label>
-        <input type="number" step="any" name="r1" required><br>
-        
-        <label for="r2">Enter the value of resistor R2 (ohms): </label>
-        <input type="number" step="any" name="r2" required><br>
-        
-        <input type="submit" value="Calculate">
+    <form action="viewing_distance.php" method="post">
+      <input type="text" name="ds" placeholder="Display diagonal size (in inches)">
+      <input type="text" name="nhr" placeholder="Display native horizontal resolution (in pixels)">
+      <input type="text" name="nvr" placeholder="Display native vertical resolution (in pixels)">
+      <input type="text" name="cvr" placeholder="Vertical resolution of the video being displayed (in pixels)">
+      <input type="submit" value="Calculate">
     </form>
 </body>
 </html>
